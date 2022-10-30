8d1ed53d771d1a4569174ffe85cb508da8364927
BashaVali <bashavali222@gmail.com>
2022-10-30 06:05:54 +0530

    Employee UC-2 Calculate Daily Employee Wage

--- EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -19,6 +19,18 @@ namespace EmployeeWageProblem
             else
                 Console.WriteLine("Employee is Absent");
         }
+        public void EmpWage()
+        {
+            int dailyEmpWage = 0, empHrs = 0;
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                empHrs = FULL_DAY_HR;
+            else
+                empHrs = 0;
+            dailyEmpWage = WAGE_PER_HR * empHrs;
+            Console.WriteLine("Daily Emp Wage Full Day :" + dailyEmpWage);
 
+
+        }
     }
 }
